a10b4ecc179f24fe926ba81bd20d6bd9d8f9173f
Jakob L. Kreuze <zerodaysfordays@sdf.org>
2020-05-31 16:06:21 -0400

    Update license information.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
-// Copyright © 2019 Jakob L. Kreuze <zerodaysfordays@sdf.lonestar.org>
+// Copyright © 2019-2020 Jakob L. Kreuze <zerodaysfordays@sdf.org>
 //
 // This file is part of mermer-rs.
 //
 // mermer-rs is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 3 of the License, or (at
-// your option) any later version.
+// under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
 //
 // mermer-rs is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// General Public License for more details.
+// Affero General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with mermer-rs. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public
+// License along with mermer-rs. If not, see <http://www.gnu.org/licenses/>.
 
 // The original C implementation of readfasta output several files:
 //
